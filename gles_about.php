cf1a6851ea7ac4ebd7985d3d771bfbf07daf1e17
saschawillems <webmaster@saschawillems.de>
2015-10-09 20:32:12 +0200

    Updated about page

---
+++ gles_about.php
@@ -0,0 +1,41 @@
+<?php 	
+	/* 		
+	*
+	* OpenGL hardware capability database server implementation
+	*	
+	* Copyright (C) 2011-2015 by Sascha Willems (www.saschawillems.de)
+	*	
+	* This code is free software, you can redistribute it and/or
+	* modify it under the terms of the GNU Affero General Public
+	* License version 3 as published by the Free Software Foundation.
+	*	
+	* Please review the following information to ensure the GNU Lesser
+	* General Public License version 3 requirements will be met:
+	* http://www.gnu.org/licenses/agpl-3.0.de.html
+	*	
+	* The code is distributed WITHOUT ANY WARRANTY; without even the
+	* implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+	* PURPOSE.  See the GNU AGPL 3.0 for more details.		
+	*
+	*/
+	include './gles_htmlheader.inc';
+	include './serverconfig/gles_config.php';	
+?>
+
+<div id='reportdiv'>	   
+	<div class="panel panel-default">
+		<div class="panel-body" style="margin-left:50px; width:65%px;">    
+			<h1>About this database</h1>
+			<p>
+				Welcome to the public (inofficial) OpenGL ES hardware database, an online tool for developers that want to check out the OpenGL hardware capabilites for the different<br>
+				graphics cards, operating systems and driver versions out there.<br><br>
+				This database and the tools to fill it are developed and maintained by me (<a href="http://www.saschawillems.de/" target="_blank">Sascha Willems</a>) in my spare time.<br>
+				No profit is made from this data, nor is this data used in any commercial way and no personal data is transferred, stored or passed.<br><br>
+				If you want to contribute to the development, you can find the source code at <a href="https://github.com/SaschaWillems/glESCapsViewerWeb" target="_blank">https://github.com/SaschaWillems/glESCapsViewerWeb</a>.
+			</p>
+		</div>    
+	</div>
+</div>
+	
+</body>
+</html>
