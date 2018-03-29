8544aec2824afd502876cec3c4ccb6bd24878a82
saschawillems <webmaster@saschawillems.de>
2018-03-29 13:22:56 +0200

    Server-side processing for report listing

---
+++ backend/reports.php
@@ -0,0 +1,183 @@
+<?php
+	/*
+		*
+		* OpenGL ES hardware capability database server implementation
+		*
+		* Copyright (C) 2011-2018 by Sascha Willems (www.saschawillems.de)
+		*
+		* This code is free software, you can redistribute it and/or
+		* modify it under the terms of the GNU Affero General Public
+		* License version 3 as published by the Free Software Foundation.
+		*
+		* Please review the following information to ensure the GNU Lesser
+		* General Public License version 3 requirements will be met:
+		* http://www.gnu.org/licenses/agpl-3.0.de.html
+		*
+		* The code is distributed WITHOUT ANY WARRANTY; without even the
+		* implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+		* PURPOSE.  See the GNU AGPL 3.0 for more details.
+		*
+	*/
+
+    include '../dbconfig.php';
+
+    function shorten($string, $length) {
+        return (strlen($string) >= $length) ? substr($string, 0, $length-10). " ... " . substr($string, -5) : $string;
+    }
+
+    DB::connect();
+
+    $data = array();
+    $params = array();    
+             
+    // Ordering
+    $orderByColumn = '';
+    $orderByDir = '';
+    if (isset($_REQUEST['order']) && count($_REQUEST['order'] > 0)) {
+        $orderByColumn = $_REQUEST['order'][0]['column'];
+        $orderByDir = $_REQUEST['order'][0]['dir'];
+    }
+
+    // Paging
+    $paging = '';
+    if (isset($_REQUEST['start'] ) && $_REQUEST['length'] != '-1') {
+        $paging = "LIMIT ".$_REQUEST["length"]. " OFFSET ".$_REQUEST["start"];
+    }  
+
+    // Filtering
+    $searchColumns = array(
+        'id', 
+        'devicename(reports.device)', 
+        'concat(reports.esversion_major, ".", reports.esversion_minor)', 
+        'concat(reports.shadinglanguageversion_major, ".", reports.shadinglanguageversion_minor)', 
+        'renderer', 
+        'os', 
+        'date(reports.submissiondate)');
+
+    // Per-column, filtering
+    $filters = array();
+    for ($i = 0; $i < count($_REQUEST['columns']); $i++) {
+        $column = $_REQUEST['columns'][$i];
+        if (($column['searchable'] == 'true') && ($column['search']['value'] != '')) {
+            $filters[] = $searchColumns[$i].' like :filter_'.$i;
+            $params['filter_'.$i] = $column['search']['value'].'%';
+            if (($i == 1) || ($i == 3)) {
+                $params['filter_'.$i] = '%'.$params['filter_'.$i];
+            }
+        }
+    }
+    if (sizeof($filters) > 0) {
+        $searchClause = 'where '.implode(' and ', $filters);
+    }        
+
+    $whereClause = '';
+    $selectAddColumns = '';
+    $negate = false;
+	if (isset($_REQUEST['filter']['option'])) {
+		if ($_REQUEST['filter']['option'] == 'not') {
+			$negate = true;
+		}
+    }        
+	// Filters
+    // GLES Extension
+	if (isset($_REQUEST['filter']['extension'])) {
+	    $extension = $_REQUEST['filter']['extension'];
+        if ($extension != '') {
+            $whereClause = "where id ".($negate ? "not" : "")." in (select distinct(reportid) from reports_extensions rext join extensions ext on ext.id = rext.EXTENSIONID where ext.name = :filter_extension)";
+            $params['filter_extension'] = $extension;
+        }
+    }
+    // EGL Extension
+	if (isset($_REQUEST['filter']['eglextension'])) {
+	    $extension = $_REQUEST['filter']['eglextension'];
+        if ($extension != '') {
+            $whereClause = "where id ".($negate ? "not" : "")." in (select distinct(reportid) from reports_eglextensions rext join egl_extensions ext on ext.id = rext.id where ext.name = :filter_extension)";
+            $params['filter_extension'] = $extension;
+        }
+    }
+
+    // Compressed format
+    if (isset($_REQUEST['filter']['compressedtextureformat'])) {
+	    $compressedformat = $_REQUEST['filter']['compressedtextureformat'];
+        if ($compressedformat != '') {
+            $whereClause = "where id ".($negate ? "not" : "")." in (select distinct(reportid) from reports_compressedformats rcf join compressedformats cf on cf.id = rcf.compressedformatid where cf.name = :filter_compressedformat)";
+            $params['filter_compressedformat'] = $compressedformat;            
+        }
+    }   
+    
+    // Device feature
+    if (isset($_REQUEST['filter']['devicefeature'])) {
+	    $feature = $_REQUEST['filter']['devicefeature'];
+        if ($feature != '') {
+            $whereClause = "where id ".($negate ? "not" : "")." in (select distinct(reportid) from reports_devicefeatures rdev join devicefeatures dev on dev.id = rdev.devicefeatureid where dev.DEVICEFEATURE = :filter_devicefeature)";
+            $params['filter_devicefeature'] = $feature;            
+        }
+    }   
+
+    // Submitter
+    if (isset($_REQUEST['filter']['submitter'])) {
+	    $submitter = $_REQUEST['filter']['submitter'];
+        if ($submitter != '') {
+            $whereClause = "where submitter = :filter_submitter";
+            $params['filter_submitter'] = $submitter;            
+        }
+    }
+
+    if (!empty($orderByColumn)) {
+        $orderBy = "order by ".$orderByColumn." ".$orderByDir;
+    }
+
+    if ($orderByColumn == "api") {
+        $orderBy = "order by length(".$orderByColumn.") ".$orderByDir.", ".$orderByColumn." ".$orderByDir;
+    }
+
+    $columns = "
+        reports.id, 
+        devicename(reports.device) as name, 
+        reports.os, 
+        concat(reports.esversion_major, '.', reports.esversion_minor) as glesversion,
+        concat(reports.shadinglanguageversion_major, '.', reports.shadinglanguageversion_minor) as slversion,
+        date(reports.submissiondate) as date, 
+        reports.gl_renderer as renderer";
+   
+    $sql = "select ".$columns." from reports ".$whereClause." ".$searchClause." ".$orderBy;
+
+    $devices = DB::$connection->prepare($sql." ".$paging);
+    $devices->execute($params);
+    if ($devices->rowCount() > 0) { 
+        foreach ($devices as $device) {            							
+            $data[] = array(
+                'id' => $device["id"], 
+                'name' => '<a href="gles_generatereport.php?reportID='.$device["id"].'">'.shorten($device["name"], 35).'</a>',
+                'glesversion' => $device["glesversion"],
+                'slversion' => $device["slversion"],
+                'renderer' => shorten($device["renderer"], 24),
+                'os' => $device["os"],
+                'date' => $device["date"],
+                'compare' => '<center><input type="checkbox" name="'.$device["id"].'" value="1"></center>'
+            );
+        }        
+    }
+
+    $filteredCount = 0;
+    $stmnt = DB::$connection->prepare("select count(*) from reports");
+    $stmnt->execute();
+    $totalCount = $stmnt->fetchColumn(); 
+
+    $filteredCount = $totalCount;
+    if (($searchClause != '') or ($whereClause != ''))  {
+        $stmnt = DB::$connection->prepare($sql);
+        $stmnt->execute($params);
+        $filteredCount = $stmnt->rowCount();     
+    }
+
+    $results = array(
+        "draw" => isset($_REQUEST['draw']) ? intval( $_REQUEST['draw'] ) : 0,        
+        "recordsTotal" => intval($totalCount),
+        "recordsFiltered" => intval($filteredCount),
+        "data" => $data);
+
+    DB::disconnect();     
+
+    echo json_encode($results);
+?>
---
+++ listreports.php
@@ -0,0 +1,199 @@
+ <?php
+	/*
+		*
+		* OpenGL ES hardware capability database server implementation
+		*
+		* Copyright (C) 2013-2018 by Sascha Willems (www.saschawillems.de)
+		*
+		* This code is free software, you can redistribute it and/or
+		* modify it under the terms of the GNU Affero General Public
+		* License version 3 as published by the Free Software Foundation.
+		*
+		* Please review the following information to ensure the GNU Lesser
+		* General Public License version 3 requirements will be met:
+		* http://www.gnu.org/licenses/agpl-3.0.de.html
+		*
+		* The code is distributed WITHOUT ANY WARRANTY; without even the
+		* implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+		* PURPOSE.  See the GNU AGPL 3.0 for more details.
+		*
+	*/ 
+ 
+	include './header.inc';
+
+	$negate = false;
+    $searchType = '';
+	$headeradd = '';
+	
+	// Header
+	$defaultHeader = true;
+	$alertText = null;	
+    $negate = false;
+	if (isset($_GET['option'])) {
+		if ($_GET['option'] == 'not') {
+			$negate = true;
+		}
+	}
+
+	// Filters
+	$filter["extension"] = null;
+	if ($_GET['extension'] != '') {
+		$filter["extension"] = $_GET['extension'];
+		$defaultHeader = false;
+		$headerClass = $negate ? "header-red" : "header-green";			
+		$caption = "Reports ".($negate ? "<b>not</b>" : "")." supporting <b>".$filter["extension"]."</b>";	
+		$caption .= " (<a href='listreports.php?extension=".$filter["extension"].($negate ? "" : "&option=not")."'>toggle</a>)";
+	}
+
+	$filter["eglextension"] = null;
+	if ($_GET['eglextension'] != '') {
+		$filter["eglextension"] = $_GET['eglextension'];
+		$defaultHeader = false;
+		$headerClass = $negate ? "header-red" : "header-green";			
+		$caption = "Reports ".($negate ? "<b>not</b>" : "")." supporting <b>".$filter["eglextension"]."</b>";	
+		$caption .= " (<a href='listreports.php?eglextension=".$filter["eglextension"].($negate ? "" : "&option=not")."'>toggle</a>)";
+	}	
+
+	$filter["compressedtextureformat"] = null;
+    if($_GET['compressedtextureformat'] != '') {
+		$filter["compressedtextureformat"] = $_GET['compressedtextureformat'];
+		$defaultHeader = false;
+		$headerClass = $negate ? "header-red" : "header-green";			
+		$caption = "Reports ".($negate ? "<b>not</b>" : "")." supporting format <b>".$filter["compressedtextureformat"]."</b>";	
+		$caption .= " (<a href='listreports.php?compressedtextureformat=".$filter["compressedtextureformat"].($negate ? "" : "&option=not")."'>toggle</a>)";
+	}
+
+	$filter["devicefeature"] = null;
+    if($_GET['devicefeature'] != '') {
+		$filter["devicefeature"] = $_GET['devicefeature'];
+		$defaultHeader = false;
+		$headerClass = $negate ? "header-red" : "header-green";			
+		$caption = "Reports ".($negate ? "<b>not</b>" : "")." supporting feature <b>".$filter["devicefeature"]."</b>";	
+		$caption .= " (<a href='listreports.php?devicefeature=".$filter["devicefeature"].($negate ? "" : "&option=not")."'>toggle</a>)";
+    }
+
+	$filter["submitter"] = null;
+    if($_GET['submitter'] != '') {
+		$filter["submitter"] = $_GET['submitter'];
+		$defaultHeader = false;
+		$headerClass = "header-blue";
+		$caption = "Reports submitted by <b>".$filter["submitter"]."</b>";	
+    }
+	
+	if ($defaultHeader) {
+		echo "<div class='header'>";	
+		echo "	<h4>Listing reports</h4>";
+		echo "</div>";		
+	}			
+?>
+
+	<center>
+	<div class="tablediv">
+		<form method="get" action="gles_comparereports.php?">
+			<table id="reports" class="table table-striped table-bordered table-hover reporttable" style="width:auto">
+				<?php
+					if (!$defaultHeader) {
+						echo "<caption class='".$headerClass." header-span'>".$caption."</caption>";
+					}
+				?>				
+				<thead>
+					<tr>
+						<th></th>
+						<th>Name</th>
+						<th>GLES</th>
+						<th>SL</th>
+						<th>Renderer</th>
+						<th>Android</th>
+						<th>Date</th>
+						<th><input type='submit' name='compare' value='compare' class='button'></th>
+					</tr>
+					<tr>
+						<th>id</th>					
+						<th>Name</th>
+						<th>GLES</th>
+						<th>SL</th>
+						<th>Renderer</th>
+						<th>Android</th>
+						<th>Date</th>
+						<th></th>
+					</tr>
+				</thead>
+			</table>
+			<div id="errordiv" style="color:#D8000C;"></div>		
+		</form>
+		</div>
+					
+	<script>
+		$( document ).ready(function() {
+
+			var table = $('#reports').DataTable({
+				"processing": true,
+				"serverSide": true,
+				"paging" : true,		
+				"searching": true,	
+				"lengthChange": false,
+				"dom": 'lrtip',	
+				"pageLength" : 25,		
+				"order": [[ 0, 'desc' ]],
+				"columnDefs": [
+					{ 
+						"searchable": false, "targets": [ 0, 7 ],
+						"orderable": false, "targets": 7,
+					}
+				],
+				"ajax": {
+					url :"backend/reports.php",
+					data: {
+						"filter": {
+							'option' : '<?php echo $_GET["option"] ?>',
+							'extension' : '<?php echo $filter["extension"] ?>',
+							'eglextension' : '<?php echo $filter["eglextension"] ?>',
+							'compressedtextureformat': '<?php echo $filter["compressedtextureformat"] ?>',
+							'devicefeature': '<?php echo $filter["devicefeature"] ?>',
+							'submitter': '<?php echo $filter["submitter"] ?>'
+						}
+					},
+					error: function (xhr, error, thrown) {
+						$('#errordiv').html('Could not fetch data (' + error + ')');
+						$('#reports_processing').hide();
+					}				
+				},
+				"columns": [
+					{ data: 'id' },
+					{ data: 'name' },
+					{ data: 'glesversion' },
+					{ data: 'slversion' },
+					{ data: 'renderer' },
+					{ data: 'os' },
+					{ data: 'date' },
+					{ data: 'compare' },				
+				],
+				// Pass order by column information to server side script
+				fnServerParams: function(data) {
+					data['order'].forEach(function(items, index) {
+						data['order'][index]['column'] = data['columns'][items.column]['data'];
+					});
+				},
+			});   
+
+			// Per-Column filter boxes
+			$('#reports thead th').each( function (i) {
+				var title = $('#reports thead th').eq( $(this).index() ).text();
+				if ((title !== 'id') && (title !== '')) {
+					var w = (title != 'Name') ? 120 : 240;
+					$(this).html( '<input type="text" placeholder="'+title+'" data-index="'+i+'" style="width: '+w+'px;" class="filterinput" />' );
+				}
+			}); 
+			$(table.table().container() ).on('keyup', 'thead input', function () {
+				table
+					.column($(this).data('index'))
+					.search(this.value)
+					.draw();
+			});		
+
+		});
+	</script>
+	<?php include("./gles_footer.inc");	?>
+	</center>	
+</body>
+</html>
